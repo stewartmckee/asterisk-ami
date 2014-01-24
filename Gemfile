a6a2efc8fd118864273c071e30635c1508506f36
Stewart McKee <stewart@theizone.co.uk>
2014-01-24 19:07:20 +0000

    switched to using EM::WebSocket

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in asterisk-ami.gemspec
 gemspec
+
+gem 'em-websocket', :git => "https://github.com/igrigorik/em-websocket.git"
--- lib/asterisk/server/socket.rb
+++ lib/asterisk/server/socket.rb
@@ -4,6 +4,8 @@
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__)) + "/../lib"
 require File.expand_path(File.dirname(__FILE__)) + "/../../ami"
 
+require 'em-websocket'
+
 Thread.abort_on_exception = true
 # WebSocket.debug = true
 
@@ -12,31 +14,61 @@ if ARGV.size != 5
   exit(1)
 end
 
+# server = WebSocketServer.new(
+#   :accepted_domains => [ARGV[0], "localhost"],
+#   :port => ARGV[1].to_i())
 
-server = WebSocketServer.new(
-  :accepted_domains => [ARGV[0], "localhost"],
-  :port => ARGV[1].to_i())
+port = ARGV[1].to_i
 
 ami_username = ARGV[2]
 ami_password = ARGV[3]
 ami_host = ARGV[4]
 
-puts("Server is running at port %d" % server.port)
-server.run() do |ws|
-  puts("Connection accepted")
-  puts("Path: #{ws.path}, Origin: #{ws.origin}")
-  if ws.path == "/"
-    puts "Received connection from browser"
-    ws.handshake()
-    ws.send("Connected.")
-    connection = Asterisk::Connection.new(ARGV[2], ARGV[3], ARGV[4])
-    connection.events do |data|
-      puts data.to_json
-      ws.send(data.to_json)
-    end
-    puts "out of loop, finished thread."
-  else
-    ws.handshake("404 Not Found")
+EM.run {
+  EM::WebSocket.run(:host => "0.0.0.0", :port => port) do |ws|
+    ws.onopen { |handshake|
+      puts "WebSocket connection open"
+
+      # Access properties on the EM::WebSocket::Handshake object, e.g.
+      # path, query_string, origin, headers
+
+      # Publish message to the client
+      ws.send "Welcome to asterisk-ami, you connected to #{handshake.path}"
+
+      connection = Asterisk::Connection.new(ARGV[2], ARGV[3], ARGV[4])
+      connection.events do |data|
+        puts data.to_json
+        ws.send(data.to_json)
+      end
+      puts "out of loop, finished thread."
+
+    }
+
+    ws.onclose { puts "Connection closed" }
+
+    ws.onmessage { |msg|
+      puts "Recieved message: #{msg}"
+      ws.send "Pong: #{msg}"
+    }
   end
-  puts("Connection closed")
-end
+}
+
+# puts("Server is running at port %d" % server.port)
+# server.run() do |ws|
+#   puts("Connection accepted")
+#   puts("Path: #{ws.path}, Origin: #{ws.origin}")
+#   if ws.path == "/"
+#     puts "Received connection from browser"
+#     ws.handshake()
+#     ws.send("Connected.")
+#     connection = Asterisk::Connection.new(ARGV[2], ARGV[3], ARGV[4])
+#     connection.events do |data|
+#       puts data.to_json
+#       ws.send(data.to_json)
+#     end
+#     puts "out of loop, finished thread."
+#   else
+#     ws.handshake("404 Not Found")
+#   end
+#   puts("Connection closed")
+# end
